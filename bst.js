4842afa440f45bb1d1e0dea1e5165ea3deed8f17
AntRod18 <antrod1895@gmail.com>
2021-12-02 14:09:45 -0500

    create node helper class for bst class

---
+++ bst.js
@@ -0,0 +1,15 @@
+class Node {
+    constructor(value){
+        this.value = value;
+        this.left = left;
+        this.right = right;
+    }
+
+    class BinarySearchTree {
+        constructor(){
+            this.root = null;
+        }
+
+        
+    }
+}
