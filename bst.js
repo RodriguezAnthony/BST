cc2c1075dc0ee26edcdc57108b3e774e41bad5a7
AntRod18 <antrod1895@gmail.com>
2021-12-02 14:24:59 -0500

    create insert function to add a node to tree

--- bst.js
+++ bst.js
@@ -4,12 +4,37 @@ class Node {
         this.left = left;
         this.right = right;
     }
-
-    class BinarySearchTree {
-        constructor(){
-            this.root = null;
-        }
-
-        
+}
+class BinarySearchTree {
+    constructor(){
+        this.root = null;
     }
+
+    insert(val){
+        let newNode = new Node(val);
+        if(this.root === null){
+            this.root = newNode;
+            return this;
+        } else{
+            let current = this.root;
+            while(true){
+                if(val === current.value) return undefined;
+                if(val < current.value){
+                    if(current.left === null){
+                        current.left = newNode;
+                        return this;
+                    } else{
+                        current = current.left;
+                    }
+                }else if(value > current.value){
+                    if(current.right === null){
+                        current.right = newNode;
+                        return this;
+                    } else{
+                        current = current.right;
+                    }
+                }
+            }
+        }
+     }
 }
